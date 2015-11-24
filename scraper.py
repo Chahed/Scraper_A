54a8d900819ee7a4351d6052610428474bad9d45
Chahed <hedi.chahed.2i@gmail.com>
2015-11-24 23:37:49 +0100

    2

--- scraper.py
+++ scraper.py
@@ -1,25 +1,24 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from splinter import Browser
+import time
+import random
 import scraperwiki
+
+def dateclean(date):
+    d=date.split('/')
+    return d[2]+'-'+d[1]+'-'+d[0]
+
+def datecleannow(date):
+    a=date.split(' ')
+    d=a[1].split('/')
+    return d[2]+'-'+d[1]+'-'+d[0]
 
 def convertirUrl(url):
     l= url.split('/')
-    newurl= l[0]+l[1]
+    newurl= l[0]+'//'+l[2]+'/'+l[3]+'/'
     return newurl
 
-def listUrl(url):
-
-    response = urlopen(url)
-    htmltext = BeautifulSoup(response)
-
-    NewUrl=convertirUrl(url)
-    soop = htmltext.find('div',{"class":"basic-table"})
-    links = soop.find_all('a')
-    href=[]
-    for i in range(0,len(links)-1):
-        href.append(NewUrl+links[i].get('href'))
-
-    return href
 
 def getId(Url):
     url= Url.split('=')
@@ -94,7 +93,10 @@ def address(Text):
 def other12(Text):
     a= Text.split("12.")
     c= a[1].split('Other Information:')
-    return c[2]
+    try:
+        return c[2]
+    except:
+        return c[1]
 
 def other13(Text):
     a= Text.split("12.")
@@ -117,8 +119,9 @@ def scrap(url):
     Text= BeautifulSoup(str(suit[3])).text
 
     Published=BeautifulSoup(str(suit[2])).text
+    Published_clean=dateclean(Published)
 
-    Titre= titre(Text)
+    Title= titre(Text)
     Awarding_Authority=Awarding(Text)
     try:
         Contact_Type =contact_type(Text)
@@ -131,6 +134,7 @@ def scrap(url):
     Reference_Attributed_by_the_Awarding_Authority=reference(Text)
     Estimated_Value_of_Requirement=estimated(Text)
     Deadline_for_Expression_of_Interest=deadline(Text)
+    Deadline_for_Expression_of_Interest_clean=datecleannow(Deadline_for_Expression_of_Interest)
     Address_to_which_they_must_be_sent=address(Text)
     try:
         Other_Information=other13(Text)
@@ -141,11 +145,11 @@ def scrap(url):
     except:
         Description_of_Amendment=""
 
-
     data={"ID":unicode(Id), \
           "Url":unicode(url),\
           "Title":unicode(Title),\
           "Published":unicode(Published),\
+          "Published clean":unicode(Published_clean),\
           "Awarding Authority":unicode(Awarding_Authority),\
           "Contact Type":unicode(Contact_Type),\
           "Decription":unicode(Decription),\
@@ -155,16 +159,98 @@ def scrap(url):
           "Reference Attributed by the Awarding Authority":unicode(Reference_Attributed_by_the_Awarding_Authority),\
           "Estimated Value of Requirement":unicode(Estimated_Value_of_Requirement),\
           "Deadline for Expression of Interest":unicode(Deadline_for_Expression_of_Interest),\
+          "Deadline for Expression of Interest clean":unicode(Deadline_for_Expression_of_Interest_clean),\
           "Address to which they must be sent":unicode(Address_to_which_they_must_be_sent),\
           "Other Information":unicode(Other_Information),\
           "Description of Amendment":unicode(Description_of_Amendment)}
     scraperwiki.sqlite.save(unique_keys=['ID'], data=data)
 
+def scrap_now(url):
+    response = urlopen(url)
+    htmltext = BeautifulSoup(response)
+    Id=getId(url)
+
+    suit =htmltext.find('div',{"id":"content1"}).findAll('label')
+    Description= BeautifulSoup(str(suit[3])).text
+    Description=Description.encode('ascii','ignore')
+
+    Published=BeautifulSoup(str(suit[2])).text
+    Published_clean=dateclean(Published)
+
+    Title=BeautifulSoup(str(suit[0])).text
+    Awarding_Authority=BeautifulSoup(str(suit[1])).text
+
+    data={"ID":unicode(Id), \
+          "Url":unicode(url),\
+          "Title":unicode(Title),\
+          "Published":unicode(Published),\
+          "Published clean":unicode(Published_clean),\
+          "Awarding Authority":unicode(Awarding_Authority),\
+          "Contact Type":unicode(),\
+          "Decription":unicode(Description),\
+          "CVP Codes":unicode(),\
+          "NUTS Codes":unicode(),\
+          "Main Site or Location of Works":unicode(),\
+          "Reference Attributed by the Awarding Authority":unicode(),\
+          "Estimated Value of Requirement":unicode(),\
+          "Deadline for Expression of Interest":unicode(),\
+          "Deadline for Expression of Interest clean":unicode(),\
+          "Address to which they must be sent":unicode(),\
+          "Other Information":unicode(),\
+          "Description of Amendment":unicode()}
+    scraperwiki.sqlite.save(unique_keys=['ID'], data=data)
+
+
+
+def Navigation(link):
+    with Browser("phantomjs", service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any']) as browser:
+        browser.driver.set_window_size(1280, 1024)
+        browser.visit(link)
+        time.sleep(random.uniform(0.5,2.9))
+        button = browser.find_by_name("editSearch")
+        button.click()
+        button = browser.find_by_name("Search")
+        button.click()
+        htmltext = BeautifulSoup(browser.html, "html.parser")
+        soop = htmltext.find('table',{"id":"noticeResults"}).findNext('tbody')
+        links = soop.findAll('a')
+        href=[]
+        for i in range(0,len(links)-1):
+            print (convertirUrl(link)+links[i].get('href'))
+            href.append(convertirUrl(link)+links[i].get('href'))
+        j=1
+        try:
+            while(1):
+                time.sleep(random.uniform(0.5,2.9))
+                j = j+1
+                page = "?page=" + str(j)
+                button = browser.find_by_css('a[href=' + '"' + page + '"' +']')
+                button.click()
+                htmltext = BeautifulSoup(browser.html, "html.parser")
+                soop = htmltext.find('table',{"id":"noticeResults"}).findNext('tbody')
+                links = soop.findAll('a')
+                for i in range(0,len(links)-1):
+                    print (convertirUrl(link)+links[i].get('href'))
+                    href.append(convertirUrl(link)+links[i].get('href'))
+
+        except:
+            pass
+    return href
+
+def main():
+    urls = ['http://noticesearch.supply4nwfire.org.uk/noticeSearch/noticeSearchResults.html?page=1']
+
+    for link in urls:
+        href=Navigation(link)
+        for i in href:
+            try:
+                scrap(i)
+            except:
+                scrap_now(i)
+
 
 
 
 if __name__ == '__main__':
-    lis=["http://noticesearch.supply4nwfire.org.uk/noticeSearch/viewNotice.html?displayNoticeId=108456102","http://noticesearch.supply4nwfire.org.uk/noticeSearch/viewNotice.html?displayNoticeId=141938986",
-    "http://noticesearch.supply4nwfire.org.uk/noticeSearch/viewNotice.html?displayNoticeId=80347654","http://noticesearch.supply4nwfire.org.uk/noticeSearch/viewNotice.html?displayNoticeId=57046877"]
-    for i in lis :
-        scrap(i)
+    main()
+
